933fdd5d8ed00cf8ed87c2bd67038d2b00c1aa35
Sigit Kurniawan <93625275+sigitKurnia1@users.noreply.github.com>
2024-05-08 15:05:12 +0700

    Delete Fullstack directory

--- Fullstack/frontend-vite-react/src/views/home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <div className="p-5 mb-4 bg-light rounded-3">
-            <div className="container-fluid py-5">
-                <h1 className="display-5 fw-bold">REACT (VITE) + LARAVEL 10</h1>
-            </div>
-        </div>
-    )
-}
